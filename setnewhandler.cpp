6ce9c8910dea22a3ac61c9c962ea53429b5a46e5
NeauCoder <386273319@qq.com>
2020-07-24 18:07:04 +0800

    create setnewhandler.cpp

---
+++ setnewhandler.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdlib>//std::exit
+#include <new>//std::set_new_handler
+
+void no_memory()
+{
+    std::cout << "Failed to allocate memory!\n";
+    std::exit();
+}
+
+int main()
+{
+    std::set_new_handler(no_memory);
+    std::cout << "Attempting to allocate 1 GiB...";
+    char* p = new char[1024*1024*1024];
+    std::cout << "OK\n";
+    delete[] p;
+    return 0;
+}
