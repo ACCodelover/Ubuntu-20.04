fab53816c3449e33eb44896c8eaa01da25994f3c
NeauCoder <386273319@qq.com>
2020-07-25 17:41:06 +0800

    copy from signal1.cpp and add the usage of func raise

---
+++ signal2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <csignal>
+#include <unistd.h>
+
+using namespace std;
+
+void singalHandler(int signum)
+{
+    cout << "Interrupt signal (" << signum << ") received.\n";
+    
+    //清理并关闭
+    //终止程序
+    exit(signum);
+}
+
+int main()
+{
+    int i = 0;
+    signal(SIGINT,singalHandler);
+    while(++1)
+    {
+        cout << "Going to sleep..." << endl;
+        if(i==3)
+            raise(SIGINT);
+        sleep(1);
+    }
+
+    return 0;
+}
