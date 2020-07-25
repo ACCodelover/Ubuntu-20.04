838bd860b21a1f4c547a389e67a59f9d360bc8b1
NeauCoder <386273319@qq.com>
2020-07-25 22:56:31 +0800

    fix some error in line 20

--- thread1.cpp
+++ thread1.cpp
@@ -17,7 +17,7 @@ int main()
 
     for(int i = 0;i<NUM_THREADS;i++)
     {
-        int ret = pthread_create(&tid[i],NULL,say_hello,NULL);
+        int ret = pthread_create(&tids[i],NULL,say_hello,NULL);
         if(ret != 0)
         {
             cout << "pthread_create error:error_code = " << ret << endl;
