02df4dc65f105060568286c9e48687c700bab6f4
NeauCoder <386273319@qq.com>
2020-07-25 17:30:19 +0800

    add # and ## usage

--- predefinemacro.cpp
+++ predefinemacro.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 using namespace std;
  
+#define concat(a,b) a ## b
+#define MKSTR(x) #x
+
 int main ()
 {
+    int xy = 100;
     cout << "Value of __LINE__ : " << __LINE__ << endl;
     cout << "Value of __FILE__ : " << __FILE__ << endl;
     cout << "Value of __DATE__ : " << __DATE__ << endl;
     cout << "Value of __TIME__ : " << __TIME__ << endl;
+
+    cout << MKSTR(HELLO C++) << endl;
  
+    cout << concat(x,y) << endl;
     return 0;
 }
