084c3dbac1f892283d909e6e3bc0adf60a78b76e
NeauCoder <386273319@qq.com>
2020-07-25 17:09:14 +0800

    replace the whole file with example code

--- template2.cpp
+++ template2.cpp
@@ -3,71 +3,68 @@
 #include <cstdlib>
 #include <string>
 #include <stdexcept>
-
+ 
 using namespace std;
-
+ 
 template <class T>
-class Stack
-{
-    private:
-    vector<T> elems;
-
-    public:
-    void push(T const&);
-    void pop();
-    T top() const;
-    bool empty() const
-    {
+class Stack {
+  private:
+    vector<T> elems;     // 元素
+ 
+  public:
+    void push(T const&);  // 入栈
+    void pop();               // 出栈
+    T top() const;            // 返回栈顶元素
+    bool empty() const{       // 如果为空则返回真。
         return elems.empty();
     }
 };
-
+ 
 template <class T>
-void Stack<T>::push(T const& elem)
+void Stack<T>::push (T const& elem)
 {
-    elems.push_back(elem);
+    // 追加传入元素的副本
+    elems.push_back(elem);   
 }
-
+ 
 template <class T>
-void Stack<T>::pop()
+void Stack<T>::pop ()
 {
-    if(elems.empty())
-    {
-        throw out_of_range("Stack<>::pop(): empty statck");
+    if (elems.empty()) {
+        throw out_of_range("Stack<>::pop(): empty stack");
     }
-    elems.pop_back();
+    // 删除最后一个元素
+    elems.pop_back();        
 }
-
+ 
 template <class T>
-T Stack<T>::top() const
+T Stack<T>::top () const
 {
-    if(elems.empty())
-    {
+    if (elems.empty()) {
         throw out_of_range("Stack<>::top(): empty stack");
     }
-
-    return elems.back();
+    // 返回最后一个元素的副本
+    return elems.back();     
 }
+ 
 int main()
 {
-    try
-    {
-        Stack<int> intStack;
-        Stack<string> stringStack;
-
+    try {
+        Stack<int>         intStack;  // int 类型的栈
+        Stack<string> stringStack;    // string 类型的栈
+ 
+        // 操作 int 类型的栈
         intStack.push(7);
-        cout << intStack.top() << endl;
-
+        cout << intStack.top() <<endl;
+ 
+        // 操作 string 类型的栈
         stringStack.push("hello");
-        cout << stringStack.top() << endl;
+        cout << stringStack.top() << std::endl;
+        stringStack.pop();
         stringStack.pop();
     }
-    catch(exception const& ex)
-    {
-        cerr << "Exception: " << ex.what() << endl;
+    catch (exception const& ex) {
+        cerr << "Exception: " << ex.what() <<endl;
         return -1;
     }
-    
-
-    return 0;
 }
